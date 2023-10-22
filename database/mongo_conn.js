bf8b5f9266f68a662d75b93fcbdb88b6193144fb
VINAY <vinayselwal@gmail.com>
2023-10-22 18:27:37 +0530

    files added

--- database/mongo_conn.js
+++ database/mongo_conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-.connect('mongodb://0.0.0.0:27017/userdata')
+.connect('mongodb+srv://vinayselwal:Vinay@123@melody.k5bfwbi.mongodb.net/')
 .then( ()=> {console.log('mongo connected')})
 .catch((err)=> {console.log('mongo error',err)})
 
